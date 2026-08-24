624817456d211d5ee799e0875832342b7d99377a
Skip <skip@usecurling.com>
2026-08-24 13:14:41 -0300

    Grant SELECT permission on legacy tables to anon and authenticated roles

---
+++ supabase/migrations/20260824161500_grant_select_legacy_tables.sql
@@ -0,0 +1,7 @@
+GRANT SELECT ON public.titulo TO anon, authenticated;
+GRANT SELECT ON public.exemplar TO anon, authenticated;
+GRANT SELECT ON public.leitor TO anon, authenticated;
+GRANT SELECT ON public.emprestimo TO anon, authenticated;
+GRANT SELECT ON public.reserva TO anon, authenticated;
+GRANT SELECT ON public.historico_movimentacao TO anon, authenticated;
+GRANT SELECT ON public.parametro_sistema TO anon, authenticated;
