87354ed5e470d6502f4c830911a03b3d210b213a
FELICIO TATSUMI ISHII <ishii7883@gmail.com>
2026-08-25 14:12:09 -0300

    Create migration to grant DML permissions on legacy tables

---
+++ supabase/migrations/20260825180000_grant_dml_legacy_tables.sql
@@ -0,0 +1,5 @@
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.titulo TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.exemplar TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.leitor TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.emprestimo TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.reserva TO anon, authenticated;
